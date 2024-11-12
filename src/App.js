aff840655a15938d6807592553bb1ed207be9324
saied-nasiripour <saied.nasiripour@gmail.com>
2024-11-12 16:53:44 +0330

    Add basic components for initial structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
 
 function App() {
-    return <div>Hello React</div>
+    const step = 1;
+
+    function handlePrevious() {
+        alert("Previous")
+    }
+
+    function handleNext() {
+        alert("Next")
+    }
+
+    return (
+        <div className="steps">
+            <div className="numbers">
+                <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
+                <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
+                <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+            </div>
+
+            <p className="message">Step {step}: {messages[step - 1]}</p>
+
+            <div className="buttons">
+                <button
+                    onClick={handlePrevious}
+                    style={{
+                        backgroundColor: "#7950f2",
+                        color: "#fff",
+                    }}
+                >Previous</button>
+                <button
+                    onClick={handleNext}
+                    style={{
+                        backgroundColor: "#7950f2",
+                        color: "#fff",
+                    }}
+                >Next</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
